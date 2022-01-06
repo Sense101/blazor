55b7dae66ba90a9a54297c053e7dff4a9164fc00
Sense101 <67970865+Sense101@users.noreply.github.com>
2022-01-06 15:44:21 +0000

    Add total price to database

---
+++ Migrations/20220106154229_AddTotalPrice.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blazor.Migrations
+{
+    public partial class AddTotalPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TotalPrice",
+                table: "Baskets",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Baskets");
+        }
+    }
+}
--- Models/Basket.cs
+++ Models/Basket.cs
@@ -3,6 +3,6 @@ namespace Blazor.Models;
 public class Basket
 {
     public int Id { get; set; }
-    public string TotalPrice;
+    public string TotalPrice { get; set; }
     public List<BasketItem> BasketItems { get; set; } = new();
 }
